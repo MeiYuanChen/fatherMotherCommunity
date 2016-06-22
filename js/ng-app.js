d93d51d4c00bdd44746a9cc7b0678ecdb3bb3281
WZJS-CMY <WZJS-CMY@chenmeiyuandeMacBook-Air.local>
2016-06-22 11:55:38 +0800

    添加vue项目文件

--- js/ng-app.js
+++ js/ng-app.js
@@ -26,6 +26,176 @@
     doc.addEventListener('DOMContentLoaded', recalc, false);
 })
 (document, window);
+
+//angular start
+var app=angular.module("myApp",[]);
+var iid=Heng.getIid();
+//获取话题信息
+var all_api="/api/parentCommunity/getViewTopicPage?pageIndex=1&pageSize=100";
+var all_url=Heng.options.base_url + all_api + "&Authorization=" + Heng.getToken();
+app.controller('allTopic',function($scope,$http){
+    $http.get(all_url).success(function(response){$scope.list=response.List;});
+
+});
+//获取token
+app.controller('gettingtoken',function($scope){
+    $scope.lh="token=" + Heng.getToken();
+});
+//html转义
+app.filter("trustHtml",function($sce){
+    return function (input){
+        return $sce.trustAsHtml(input);
+    }
+});
+//获取微信用户信息
+var weixin_userinfo_api = "/api/weixin/userInfo";
+var url = Heng.options.base_url + weixin_userinfo_api + "?Authorization=" + Heng.getToken();// 请求微信用户信息地址
+app.controller('weixin',function($scope,$http){
+
+    $http.get(url).success(
+        function(response) {
+            $scope.weixin=response.Result;
+        }
+    );
+
+
+});
+//当前话题信息
+var topicInfo_api="/api/parentCommunity/getMyViewTopic?topicId="+iid;
+var topicInfo_url=Heng.options.base_url + topicInfo_api + "&Authorization=" + Heng.getToken();
+//获取评论
+var startcommentpage= 1,endcommentpage=20;
+var comment_api="/api/parentCommunity/getReplyPage?topicId="+iid+"&pageIndex="+startcommentpage+"&pageSize="+endcommentpage;
+var comment_url=Heng.options.base_url + comment_api + "&Authorization=" + Heng.getToken();
+app.controller('topicInfo',function($scope,$http){
+    $http.get(topicInfo_url).success(function(response){$scope.result=response.Result;});
+    $http.get(comment_url).success(function(response){
+        $scope.list=response.List;
+        $scope.count=response.Count;
+        //加载更多
+        if($scope.count>=20){
+            angular.element("#morebtn").show();
+        }else{
+            angular.element("#morebtn").hide();
+        }
+    });
+    //点击返回按钮时 更新列表数据
+    $scope.back=function(){
+        $http.get(all_url).success(function(response){$scope.list=response.List;});
+    };
+    //跑票
+    $scope.updateRunTicket=function(RId){
+        var updateRunTicket_api="/api/parentCommunity/updateRunTicket?topicId="+RId;
+        var updateRunTicket_url=Heng.options.base_url + updateRunTicket_api + "&Authorization=" + Heng.getToken();
+        $http.get(updateRunTicket_url).success(function(){
+            console.log('跑票成功');
+            $http.get(topicInfo_url).success(function(response){$scope.result=response.Result;});
+        }).error(function(res){
+            alert('跑票失败'+res);
+        });
+    };
+    //投票
+    $scope.updatePoll=function(RId,rk){
+
+        var updatePoll_url=Heng.options.base_url+"/api/parentCommunity/updatePoll"+"?Authorization="+Heng.getToken();
+        var updatePoll_add={
+            "Id":0,
+            "RelationId":RId,
+            "AdderId":0,
+            "RelationKind":rk//评论类型
+        };
+        $http({
+            method:"POST",
+            url:updatePoll_url,
+            data:updatePoll_add,
+            headers:{ 'Content-Type': 'application/json'}
+        }).success(function(){
+
+            console.log("ReplyKind:"+rk);
+            if(rk==2){
+                rk="反方";
+            }else{
+                rk="正方";
+            }
+            console.log("投票成功,你投给了"+rk);
+            $http.get(topicInfo_url).success(function(response){$scope.result=response.Result;});
+        }).error(function(res){
+            alert("更新失败"+res);
+        });
+    };
+    //发表评论
+    $scope.sub=function(RId,content,name,header,rk){
+        var Stext=angular.element("#textarea .Stext");
+        var Otext=angular.element("#textarea .Otext");
+
+        if(!content){
+            return false;
+        }
+        if(rk==1){
+            if(Stext.val()==""){
+                return false;
+            }
+        }
+        if(rk==2){
+            if(Otext.val()==""){
+                return false;
+            }
+        }
+        var postcomment_url=Heng.options.base_url+"/api/parentCommunity/updateReply"+"?Authorization="+Heng.getToken();
+        var postcomment_add={
+            "Id":0,
+            "RelationId":RId,
+            "Content":content,
+            "AdderId":0,
+            "NickName": name,//添加者
+            "UserLogo":header,//添加者头像
+            "ReplyKind":rk//评论类型
+        };
+        $http({
+            method:"POST",
+            url:postcomment_url,
+            data:postcomment_add,
+            headers:{ 'Content-Type': 'application/json'}
+        }).success(function(){
+            console.log("发布评论成功");
+            Stext.val("");
+            Otext.val("");
+            $http.get(comment_url).success(function(response){$scope.list=response.List;$scope.count=response.Count;});
+
+        }).error(function(res){
+            Stext.val("");
+            Otext.val("");
+            alert('更新失败'+res);
+        });
+    };
+
+    //加载更多
+
+    $scope.readMore=function(){
+
+        endcommentpage=endcommentpage+20;
+        comment_api="/api/parentCommunity/getReplyPage?topicId="+iid+"&pageIndex="+startcommentpage+"&pageSize="+endcommentpage;
+        comment_url=Heng.options.base_url + comment_api + "&Authorization=" + Heng.getToken();
+        setTimeout(function(){
+            $http.get(comment_url).success(function(response){
+                $scope.list=response.List;$scope.count=response.Count;
+                var hided=$scope.count-endcommentpage;
+                if(hided<0){
+                    angular.element("#morebtn").hide();
+                }else{
+                    angular.element("#morebtn").show();
+                }
+            });
+        },100);
+
+    };
+
+});
+
+
+//angular end
+
+
 var CMY={};
 CMY.hide=function(e,time){
     return $(e).fadeOut(time);
@@ -36,11 +206,13 @@ CMY.show=function(e,time){
 };
 
 $(function(){
-    updateEndTime();
-    progressBar();
-    shownum();
+    WXConfig('5693024816932578630');
     backtrack();
-    hidePoll();
+    setTimeout(function(){
+        updateEndTime();
+        progressBar();
+        hidePoll();
+    },500);
 });
 var $sum;
 
@@ -95,11 +267,14 @@ function updateEndTime()
 function progressBarbtnChange(btn){
     var $left=$("#progressBarbtnL");
     var $right=$("#progressBarbtnR");
+
     if(btn=="#Svote2"||btn=="#Svote"){
+
         $left.animate({opacity:"1"},1);
         setTimeout(function(){
             $left.animate({opacity:"0"},500);
         },100);
+
     }else{
         $right.animate({opacity:"1"},1);
         setTimeout(function(){
@@ -113,7 +288,8 @@ function hoverbtnfun(btn){ //正方发表按钮
     $btnlast.attr("style","display:none");
     setTimeout(function(){
         $btnlast.attr("style","display:block");
-        $("textarea").val("");
+
+        progressBar();
     },100);
 
 
@@ -122,12 +298,14 @@ function verification(text){//验证文本框是否为空
     var reg1=/\s/;//空白行
     var $text=$(text).val();
     if(reg1.test($text)|| $text==""){
+
         CMY.show("#error","slow");
         setTimeout(function(){
 
             CMY.hide("#error","slow");
         },3000);
     }else{
+
         CMY.show("#success","slow");
         setTimeout(function(){
 
@@ -139,8 +317,10 @@ function verification(text){//验证文本框是否为空
 }
 
 function removeOnclick(removeclick1,removeclick2){
-    $(removeclick1).attr("onclick","");
-    $(removeclick2).attr("onclick","");
+    $(removeclick1).removeAttr("onclick");
+    $(removeclick2).removeAttr("onclick");
+    $(removeclick1).attr("ng-click",'');
+    $(removeclick2).attr("ng-click",'');
 }
 
 function toggleSO2(btn,add){
@@ -157,10 +337,17 @@ function toggleSO2(btn,add){
 //跑票按钮
 //backtrack("#backtrack","#Ovote","#Svote","class","Svote vote","Svoted vote","Ovote vote","Ovoted vote","#Svotediv .add","#Ovotediv .add","#Svotediv .less","#Ovotediv .less");
 function backtrack(){
+
     var $o=$("#Ovote");
     var $s=$("#Svote");
     var $Ostyle=$o.find('.img:last-child');
     var $Sstyle=$s.find('.img:last-child');
+    var $sdataVal,$odataVal;
+    setTimeout(function(){
+        $sdataVal=$("span[data-Soddnum]").attr("data-Soddnum");
+        $odataVal=$("span[data-Ooddnum]").attr("data-Ooddnum");
+    },100);
+
     $("#backtrack").click(function(){
 
         if($Ostyle.attr("style")=="display: none;"){
@@ -169,7 +356,7 @@ function backtrack(){
             progressBarbtnChange("#Svote");
             addfun($Sstyle.attr("style"),"#Svotediv .add","#Svote");
             lessfun($Ostyle.attr("style"),"#Ovotediv .less");
-            addData('')
+
         }else {
             $Ostyle.attr('style',"display: none;");
             $Sstyle.attr('style',"display: block;");
@@ -177,7 +364,11 @@ function backtrack(){
             addfun($Ostyle.attr("style"),"#Ovotediv .add","#Ovote");
             lessfun($Sstyle.attr("style"),"#Svotediv .less");
 
+
         }
+        setTimeout(function(){
+            progressBar();
+        },100);
 
     });
 
@@ -186,10 +377,10 @@ function backtrack(){
 function lessfun(a,less){
     var $lessbtn=$(less);
     if(a=="display: block;"){
-        $lessbtn.animate({top:".2rem",opacity:"1"},1);
+        $lessbtn.animate({top:".2rem",opacity:"1"},2);
         setTimeout(function(){
-            $lessbtn.animate({opacity:"0",top:"1rem"},500);
-        },2);
+            $lessbtn.animate({opacity:"0",top:"1rem"},1000);
+        },50);
     }
 }
 
@@ -198,11 +389,11 @@ function addfun(a,add,btn){
     var $addbtn=$(add);
 
     if(a=="display: none;"){
-        $addbtn.animate({top:".2rem",opacity:"1"},1);
+        $addbtn.animate({top:".2rem",opacity:"1"},2);
         setTimeout(function(){
-            $addbtn.animate({opacity:"0",top:"1rem"},500);
-        },2);
-        addData(btn);
+            $addbtn.animate({opacity:"0",top:"1rem"},1000);
+        },50);
+        //addData(btn);
     }
 
 
@@ -214,7 +405,7 @@ function addData(btn){
         shownum(1,0);
     }
 }
-//angular数据接上之后可能删掉,用于显示info的数字
+//显示info的数字
 function shownum(Sadd,Oadd){
     var $Soddnum,$Ooddnum,
         $sdata=$("span[data-Soddnum]"),
@@ -298,7 +489,7 @@ function progressBar(){
 function progressBarbtnPosition(leftWidth){
     var unit='%',//单位
         $btn=$("#progressBarbtn");
-    $btn.css('left',leftWidth-17+unit);
+    $btn.css('left',leftWidth-20+unit);
 }
 
 var browser = {
@@ -347,3 +538,30 @@ function hidePoll(){
         $('#OvotedivEnd').show();
     }
 }
+
+//微信分享
+var title = '爸妈生活链';
+var description = '爸妈生活链';
+var logo = 'http://site.jxt189.com/nledu/images/HGshare.jpg';
+
+var sharedLink = 'http://dwz.cn/3dfWEc';
+
+
+wx.ready(function () {
+    var wechatShareUri = {
+        title: title,
+        desc: description,
+        link: sharedLink,
+        imgUrl: logo
+    };
+    var momentsShareUri = {
+        title: title,
+        desc: description,
+        link: sharedLink,
+        imgUrl: logo
+    };
+    wx.onMenuShareAppMessage(wechatShareUri);
+    wx.onMenuShareTimeline(momentsShareUri);
+    HideMenuItems();
+});
+
